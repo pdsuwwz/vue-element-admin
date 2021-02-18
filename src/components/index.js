f3c7f174aba26e149acccbc675b48846bb3cf05e
GGupzHH <1248023357@qq.com>
2021-02-18 14:52:05 +0800

    feature(merge)

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// Vue Component Install
+import IconFont from '@/components/IconFont'
+
+const component = {
+  install: function (Vue) {
+    Vue.component(IconFont.name, IconFont)
+  }
+}
+
+export default component
---
+++ src/modules/UserAccount/pages/setting.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrap-user-setting">
+    账号设置
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UserAccountSetting',
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  computed: {},
+  watch: {},
+  beforeCreate () {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang='scss' scoped>
+//@import url(); 引入公共css类
+.wrap-user-setting {
+
+}
+</style>
